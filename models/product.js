3ca389d93c2128254a5c9f94f64f2294e805be10
Alviando <restualviando28@gmail.com>
2023-02-20 09:30:50 +0700

    [ADD] left join

--- models/product.js
+++ models/product.js
@@ -3,22 +3,27 @@ const db = require("../database/index");
 const updatedAt = new Date();
 
 const getAll = async () => {
-  return await db`SELECT products.*, product_images.* FROM products LEFT JOIN product_images ON products.id=product_images.product_id`;
+  return await db`SELECT products.*, categories.id as category_id, categories.category_name as category_name,  users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id LEFT JOIN categories ON products.category_id=categories.id`;
 };
 
 const getProductsById = async (id) => {
-  return await db`SELECT * FROM products WHERE id=${id}`;
+  return await db`SELECT products.*, categories.id as category_id, categories.category_name as category_name,  users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id LEFT JOIN categories ON products.category_id=categories.id WHERE products.id=${id}`;
 };
 
 const getProductsByUserId = async (user_id) => {
-  return await db`SELECT * FROM products WHERE user_id=${user_id}`;
+  return await db`SELECT products.*, categories.id as category_id, categories.category_name as category_name,  users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id LEFT JOIN categories ON products.category_id=categories.id WHERE products.user_id=${user_id}`;
+  // return await db`SELECT products.*,users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id `;
+};
+
+const getProductByCategoryId = async (category_id) => {
+  return await db`SELECT products.*, categories.id as category_id, categories.category_name as category_name,  users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id LEFT JOIN categories ON products.category_id=categories.id WHERE category_id=${category_id}`;
 };
 
 const search = async (params) => {
   const { keyword, limit, page } = params;
-  return await db`SELECT * FROM products WHERE product_name ILIKE ${
+  return await db`SELECT products.*, categories.id as category_id, categories.category_name as category_name,  users.store_name as store_name FROM products LEFT JOIN users ON products.user_id=users.id LEFT JOIN categories ON products.category_id=categories.id WHERE products.product_name ILIKE ${
     "%" + keyword + "%"
-  } ORDER BY id DESC LIMIT ${limit ?? null} OFFSET ${
+  } ORDER BY products.id DESC LIMIT ${limit ?? null} OFFSET ${
     page ? limit * (page - 1) : 0
   }`;
 };
@@ -46,4 +51,5 @@ module.exports = {
   getProductsByUserId,
   search,
   create,
+  getProductByCategoryId,
 };
