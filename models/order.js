06182452cc1b3c120e1879e8c05ce7abcf6e2bf3
Alviando <restualviando28@gmail.com>
2023-03-03 01:39:19 +0700

    [UPDATE] join users response for products

--- models/order.js
+++ models/order.js
@@ -8,7 +8,7 @@ const create = async (params) => {
 };
 
 const getOrderById = async (id) => {
-  return await db`SELECT orders.*, orders.id as orders_id, products.*, products.id as product_id, categories.*, categories.id as category_id,
+  return await db`SELECT orders.*, orders.id as orders_id, products.*, products.id as product_id, categories.*, categories.id as category_id, users.store_name as store_name, users.id as store_id,
   (
     SELECT COALESCE(json_agg(row_to_json(product_images.*)), '[]'::json) 
     FROM product_images 
@@ -16,13 +16,14 @@ const getOrderById = async (id) => {
   ) as product_images
   FROM orders LEFT JOIN products ON products.id = orders.product_id
   JOIN categories ON categories.id = products.category_id
+  JOIN users ON users.id = products.user_id
   WHERE orders.id=${id}
   ORDER BY orders.created_at DESC`;
   // return await db`SELECT * FROM orders WHERE id=${id}`;
 };
 
 const getOrderByUserId = async (user_id) => {
-  return await db`SELECT orders.*, orders.id as orders_id, products.*, products.id as product_id, categories.*, categories.id as category_id,
+  return await db`SELECT orders.*, orders.id as orders_id, products.*, products.id as product_id, categories.*, categories.id as category_id, users.store_name as store_name, users.id as store_id,
   (
     SELECT COALESCE(json_agg(row_to_json(product_images.*)), '[]'::json) 
     FROM product_images 
@@ -30,6 +31,7 @@ const getOrderByUserId = async (user_id) => {
   ) as product_images
   FROM orders LEFT JOIN products ON products.id = orders.product_id
   JOIN categories ON categories.id = products.category_id
+  JOIN users ON users.id = products.user_id
   WHERE orders.user_id=${user_id} 
   ORDER BY orders.created_at DESC`;
 };
